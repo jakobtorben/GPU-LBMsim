a31b87b50f61c3eb4f161d7af98aa42a383a5b68
acse-jrt3817 <jrt3817@ic.ac.uk>
2021-06-04 01:35:22 +0100

    Add working LBGK model

--- main.cpp
+++ main.cpp
@@ -1,62 +1,161 @@
 #include <iostream>
 
-#include "LBM.h"
 #include "utils.h"
 
-
 using namespace std;
-double** grid, * grid_1d;
-double** old_grid, * old_grid_1d;
-double** new_grid, * new_grid_1d;
 
-
-int rows, columns;							// size of whole domain
 
 // define constants
-int imax = 5, jmax = 5;
-double t_max = 10.0;
-double t = 0.0, t_out = 0.0, dt_out = 0.04, dt;
-double y_max = 10.0, x_max = 10.0, dx, dy;
+const int Nx = 10, Ny = 10;	// grid size
+const int Q = 9;			// number of velocity components 
+float rho0 = 100;			// average density
+float tau = 0.6;			// collision timescale
+int iterations = 10;		// number of iteratinos to run
+// dx = 1, dt = 1, c_s = 1 assumed throughout
+
+// weights
+float wi[Q] = { 4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36 };
+
+// neighbour nodes
+int ex[Q] = { 0, 1, 0, -1,  0, 1, -1, -1,  1 };
+int ey[Q] = { 0, 0, 1,  0, -1, 1,  1, -1, -1 };
+
+// allocate grid
+float* f =     new float[Nx * Ny * Q];
+float* ftemp = new float[Nx * Ny * Q];
+float* feq =   new float[Nx * Ny * Q];
+float* u_x =   new float[Nx * Ny];
+float* u_y =   new float[Nx * Ny];
+float* rho =   new float[Nx * Ny];
+
 
 int main()
 {
-	// allocate grid padded with ghost cells
-	setup_continuous_array(grid, grid_1d, rows + 2, columns + 2);
-	setup_continuous_array(old_grid, old_grid_1d, rows + 2, columns + 2);
-	setup_continuous_array(new_grid, new_grid_1d, rows + 2, columns + 2);
 
-	dx = x_max / ((double)imax - 1);
-	dy = y_max / ((double)jmax - 1);
+	// apply initial conditions - flow to the rigth
+	for (int i = 0; i < Nx; i++)
+		for (int j = 0; j < Ny; j++)
+			for (int a = 0; a < 9; a++)
+				f[i + Ny*(j + Q*a)] = (a != 1) ? 1.0 : 2.0;
 
-	dt = 0.1 * min(dx, dy);
-
-	int out_cnt = 0, it = 0;
-
-	while (t < t_max)
-	{
-		iteration();
-
-
-
-		t += dt;
-		std::swap(old_grid, new_grid);
-		std::swap(old_grid, grid);
-
-
-		if (t_out <= t)
+	// scale distribution
+	for (int i = 0; i < Nx; i++)
+		for (int j = 0; j < Ny; j++)
 		{
-			cout << "output: " << out_cnt << "\tt: " << t << "\titeration: " << it << endl;
-			grid_to_file(out_cnt, new_grid, rows, columns);
+			rho[i + Ny*j] = 0.0;
+			for (int a = 0; a < Q; a++)
+				rho[i + Ny*j] += f[i + Ny*(j + Q*a)];
+			for (int a = 0; a < Q; a++)
+				f[i + Ny*(j + Q*a)] *= rho0 / rho[i + Ny*j];
+			
+		}
+
+
+	// simulation main loop
+	int it = 0, out_cnt = 0;
+	while (it < iterations)
+	{
+		// calculate macroscopic quantities
+		for (int i = 0; i < Nx; i++)
+			for (int j = 0; j < Ny; j++)
+			{
+				u_x[i + Ny*j] = 0.0;
+				u_y[i + Ny*j] = 0.0;
+				rho[i + Ny*j] = 0.0;
+				for (int a = 0; a < Q; a++)
+				{
+					u_x[i + Ny*j] += f[i + Ny*(j + Q*a)];
+					u_y[i + Ny*j] += f[i + Ny*(j + Q*a)];
+					rho[i + Ny*j] += f[i + Ny*(j + Q*a)];
+				}
+				u_x[i + Ny*j] /= rho[i + Ny*j];
+				u_y[i + Ny*j] /= rho[i + Ny*j];
+			}
+		
+		// streaming step - periodic boundary conditions
+		for (int i = 0; i < Nx; i++)
+		{
+			int in = (i>0   ) ? (i-1) : (Nx-1);
+			int ip = (i<Nx-1) ? (i+1) : (0   );
+
+			for (int j = 0; j < Ny; j++)
+			{
+				int jn = (j>0   ) ? (j-1) : (Ny-1);
+				int jp = (j<Ny-1) ? (j+1) : (Ny-1);
+
+				ftemp[i  + Ny*(j     )] = f[i + Ny*(j    )];
+				ftemp[ip + Ny*(j  + 1)] = f[i + Ny*(j + 1)];
+				ftemp[i  + Ny*(jp + 2)] = f[i + Ny*(j + 2)];
+				ftemp[in + Ny*(j  + 3)] = f[i + Ny*(j + 3)];
+				ftemp[i  + Ny*(jn + 4)] = f[i + Ny*(j + 4)];
+				ftemp[ip + Ny*(jp + 5)] = f[i + Ny*(j + 5)];
+				ftemp[in + Ny*(jp + 6)] = f[i + Ny*(j + 6)];
+				ftemp[in + Ny*(jn + 7)] = f[i + Ny*(j + 7)];
+				ftemp[ip + Ny*(jn + 8)] = f[i + Ny*(j + 8)];
+			}
+		}
+
+		// calculate equilibrium distribution feq
+		float c1 = 3.0;
+		float c2 = 9./2.;
+		float c3 = 3./2.;
+		for (int i = 0; i < Nx; i++)
+			for (int j = 0; j < Ny; j++)
+			{
+				float rhoij = rho[i + Ny*j];
+				float w_rho0 = 4./9.  * rhoij;
+				float w_rho1 = 1./9.  * rhoij;
+				float w_rho2 = 1./36. * rhoij;
+
+				float uxij = u_x[i + Ny*j];
+				float uyij = u_y[i + Ny*j];
+
+				float uxsq = uxij * uxij;
+				float uysq = uyij * uyij;
+				float usq  =  uxsq * uysq;
+
+				float uxuy5 =  uxij + uyij;
+				float uxuy6 = -uxij + uyij;
+				float uxuy7 = -uxij - uyij;
+				float uxuy8 =  uxij - uyij;
+
+				float c4 = c3*usq;
+
+				feq[i + Ny*(j      )] = w_rho0*(1.                             - c4);
+				feq[i + Ny*(j + Q*1)] = w_rho1*(1. + c1*uxij  + c2*uxsq        - c4);
+				feq[i + Ny*(j + Q*2)] = w_rho1*(1. - c1*uyij  + c2*uysq        - c4);
+				feq[i + Ny*(j + Q*3)] = w_rho1*(1. - c1*uxij  + c2*uxsq        - c4);
+				feq[i + Ny*(j + Q*4)] = w_rho1*(1. + c1*uyij  + c2*uysq        - c4);
+				feq[i + Ny*(j + Q*5)] = w_rho2*(1. + c1*uxuy5 + c2*uxuy5*uxuy5 - c4);
+				feq[i + Ny*(j + Q*6)] = w_rho2*(1. + c1*uxuy6 + c2*uxuy6*uxuy6 - c4);
+				feq[i + Ny*(j + Q*7)] = w_rho2*(1. + c1*uxuy7 + c2*uxuy7*uxuy7 - c4);
+				feq[i + Ny*(j + Q*8)] = w_rho2*(1. + c1*uxuy8 + c2*uxuy8*uxuy8 - c4);
+			}
+
+		// collision step
+		for (int i = 0; i < Nx; i++)
+			for (int j = 0; j < Ny; j++)
+				for (int a = 0; a < 9; a++)
+				{
+					int ija = i + Ny*(j + Q*a);
+					f[ija] = ftemp[ija] - (ftemp[ija] - feq[ija]) / tau;
+				}
+
+		if (it % 1 == 0)
+		{
+			cout << "iteration: " << it << "\toutput: " << out_cnt << endl;
+			grid_to_file(out_cnt, u_x, u_y, Nx, Ny);
 			out_cnt++;
-			t_out += dt_out;
 		}
 		it++;
 	}
 
 
-	free_continuous_array(grid, grid_1d);
-	free_continuous_array(old_grid, old_grid_1d);
-	free_continuous_array(new_grid, new_grid_1d);
-
+	delete[] f;
+	delete[] ftemp;
+	delete[] feq;
+	delete[] u_x;
+	delete[] u_y;
+	delete[] rho;
 
 }
--- utils.h
+++ utils.h
@@ -2,37 +2,7 @@
 
 #include <sstream>
 #include <fstream>
-
-/***********************************************************
-*  **Function**: setup_continuous_array\n
-*  **Description**: Sets up arrays that will be used for
-*                   for grids, using a double pointer that
-*                   points to a 1D array.
-*  **Param**: double**& array_2d, double*& array_1d, int m, int n
-************************************************************/
-void setup_continuous_array(double**& array_2d, double*& array_1d, int m, int n)
-{
-	array_1d = new double[m * n];
-	array_2d = new double* [m];
-
-	for (int i = 0; i < m; i++)
-	{
-		array_2d[i] = &array_1d[i * n];						//index = i*n + j
-		for (int j = 0; j < n; j++)  // remove later when setup ghost cells
-			array_2d[i][j] = 0;
-	}
-}
-
-/***********************************************************
-*  **Function**: free_continuous_array\n
-*  **Description**: Deletes dynamically allocated arrays
-*  **Param**: double**& array_2d, double*& array_1d
-************************************************************/
-void free_continuous_array(double**& array_2d, double*& array_1d)
-{
-	delete[] array_1d;
-	delete[] array_2d;
-}
+#include <cmath>
 
 
 /***********************************************************
@@ -40,16 +10,20 @@ void free_continuous_array(double**& array_2d, double*& array_1d)
 *  **Description**: Saves the grid to a datafile
 *  **Param**: int out
 ************************************************************/
-void grid_to_file(int it, double** grid, int rows, int columns)
+void grid_to_file(int it, float* u_x, float* u_y, int rows, int columns)
 {
 	std::stringstream fname;
 	std::fstream f1;
 	fname << "./out/" << "output" << "_" << it << ".dat";
 	f1.open(fname.str().c_str(), std::ios_base::out);
-	for (int i = 1; i < rows + 1; i++)
+
+	for (int i = 0; i < rows; i++)
 	{
-		for (int j = 1; j < columns + 1; j++)
-			f1 << grid[i][j] << '\t';
+		for (int j = 0; j < columns; j++)
+		{
+			float vel = std::sqrt(u_x[i + columns*j] + u_y[i + columns*j]);
+			f1 << vel  << '\t';
+		}
 		f1 << '\n';
 	}
 	f1.close();
