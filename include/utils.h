d4fccb4dde9d0fe8129014b40716bae286de063e
acse-jrt3817 <jrt3817@ic.ac.uk>
2021-06-09 15:26:00 +0100

    Fix indexing bugs

--- include/utils.h
+++ include/utils.h
@@ -7,24 +7,41 @@
 
 /***********************************************************
 *  **Function**: grid_to_file\n
-*  **Description**: Saves the grid to a datafile
+*  **Description**: Saves the grid to a VTK datafile
 *  **Param**: int out
 ************************************************************/
-void grid_to_file(int it, float* u_x, float* u_y, int rows, int columns)
+void grid_to_file(int it, float* u_x, float* u_y, int Nx, int Ny)
 {
 	std::stringstream fname;
 	std::fstream f1;
-	fname << "./out/" << "output" << "_" << it << ".dat";
+	fname << "./out/" << "output" << "_" << it << ".vtk";
 	f1.open(fname.str().c_str(), std::ios_base::out);
 
-	for (int i = 0; i < rows; i++)
+	// write header
+	f1 << "# vtk DataFile Version 5.1\n";
+	f1 << "Lattice_Boltzmann_fluid_flow\n";
+	f1 << "ASCII\n";
+	f1 << "DATASET RECTILINEAR_GRID\n";
+	f1 << "DIMENSIONS " << Nx << " " << Ny << " 1 \n";
+	f1 << "X_COORDINATES " << Nx << " float\n";
+	for (int i = 0; i < Nx; i++)
+		f1 << i << " ";
+	f1 << "\nY_COORDINATES " << Ny << " float\n";
+	for (int j = 0; j < Ny; j++)
+		f1 << j << " ";
+	f1 << "\nZ_COORDINATES 1 float\n";
+	f1 << "0\n";
+	f1 << "POINT_DATA " << Nx*Ny << "\n\n";
+	f1 << "VECTORS VecVelocity float\n";
+	
+	for (int j = 0; j < Ny; j++)
 	{
-		for (int j = 0; j < columns; j++)
+		for (int i = 0; i < Nx; i++)
 		{
-			float vel = std::sqrt(u_x[i + columns*j]*u_x[i + columns*j] + u_y[i + columns*j]*u_y[i + columns*j]);
-			f1 << vel  << '\t';
+			int pos = i + Nx*j;
+			//float vel = std::sqrt(u_x[i + columns*j]*u_x[i + columns*j] + u_y[i + columns*j]*u_y[i + columns*j]);
+			f1 << u_x[pos] << " " << u_y[pos] << " 0.0 \n";
 		}
-		f1 << '\n';
 	}
 	f1.close();
 }
--- main.cpp
+++ main.cpp
@@ -5,27 +5,25 @@
 
 using namespace std;
 
-#define PI 3.14159265
-
-
 
 // define constants
-const int Nx = 100, Ny = 100;	// grid size
+const int Nx = 300, Ny = 150;	// grid size
 const int Q = 9;			// number of velocity components 
-float tau = 0.6;			// collision timescale
-int iterations = 10;		// number of iteratinos to run
-// dx = 1, dt = 1, c_s = 1 assumed throughout
+const float tau = 0.6;			// collision timescale
+const float ux0 = 1.;				// inital speed in x direction
+//const float cs2 = 1./3.;	// speed of sound**2 D2Q9
+const int iterations = 5000;		// number of iteratinos to run
+// dx = 1, dt = 1, c = 1 assumed throughout
 
 // define geometry
-int cx = 50, cy = 50;
-int radius = 20;
+const int cx = 75, cy = 75;
+const int radius = 50;
 
 // velocity components
-int ex[Q] = { 0, 1, 0, -1,  0, 1, -1, -1,  1 };
-int ey[Q] = { 0, 0, 1,  0, -1, 1,  1, -1, -1 };
+const int ex[Q] = { 0, 1, 0, -1,  0, 1, -1, -1,  1 };
+const int ey[Q] = { 0, 0, 1,  0, -1, 1,  1, -1, -1 };
 
 // allocate grid
-
 float* f          = new float[Nx * Ny * Q];
 float* ftemp      = new float[Nx * Ny * Q];
 float* feq        = new float[Nx * Ny * Q];
@@ -35,137 +33,173 @@ float* u_x        = new float[Nx * Ny];
 float* u_y        = new float[Nx * Ny];
 float* rho        = new float[Nx * Ny];
 
+// define functions
+void calc_eq(float* rho, float* u_x, float* u_y, float* result)
+{
+	// calculate equilibrium distribution feq
+	float c1 = 3.;
+	float c2 = 9./2.;
+	float c3 = 3./2.;
+	for (int j = 0; j < Ny; j++)
+		for (int i = 0; i < Nx; i++)
+		{
+			int pos = i + Nx*j;
+			if (!solid_node[pos])
+			{
+				float rhoij = rho[pos];
+				float w_rho0 = 4./9.  * rhoij;
+				float w_rho1 = 1./9.  * rhoij;
+				float w_rho2 = 1./36. * rhoij;
+
+				float uxij = u_x[pos];
+				float uyij = u_y[pos];
+
+				float uxsq = uxij * uxij;
+				float uysq = uyij * uyij;
+				float usq = uxsq + uysq;
+
+				float uxuy5 = uxij + uyij;
+				float uxuy6 = -uxij + uyij;
+				float uxuy7 = -uxij - uyij;
+				float uxuy8 = uxij - uyij;
+
+				float c4 = c3*usq;
+
+				// note that c = 1
+				result[Q*pos    ] = w_rho0*(1.                             - c4);
+				result[Q*pos + 1] = w_rho1*(1. + c1*uxij  + c2*uxsq        - c4);
+				result[Q*pos + 2] = w_rho1*(1. + c1*uyij  + c2*uysq        - c4);
+				result[Q*pos + 3] = w_rho1*(1. - c1*uxij  + c2*uxsq        - c4);
+				result[Q*pos + 4] = w_rho1*(1. - c1*uyij  + c2*uysq        - c4);
+				result[Q*pos + 5] = w_rho2*(1. + c1*uxuy5 + c2*uxuy5*uxuy5 - c4);
+				result[Q*pos + 6] = w_rho2*(1. + c1*uxuy6 + c2*uxuy6*uxuy6 - c4);
+				result[Q*pos + 7] = w_rho2*(1. + c1*uxuy7 + c2*uxuy7*uxuy7 - c4);
+				result[Q*pos + 8] = w_rho2*(1. + c1*uxuy8 + c2*uxuy8*uxuy8 - c4);
+			}
+		}
+}
+
 
 int main()
 {
-	// define geometry - cylinder with r = 5
-	for (int i = 0; i < Nx; i++)
-		for (int j = 0; j < Ny; j++)
+	// define geometry
+	for (int j = 0; j < Ny; j++)
+		for (int i = 0; i < Nx; i++)
 		{
+			int pos = i + Nx*j;
 			float dx = std::abs(cx - i);
 			float dy = std::abs(cy - j);
 			float dist = std::sqrt(dx*dx + dy*dy);
-			solid_node[i + Ny*j] = (dist < radius) ? 1 : 0;
+			solid_node[pos] = (dist < radius) ? 1 : 0;
+			if (j == 0 || j == Ny-1)
+				solid_node[pos] = 1;
 		}
 
-
 	// apply initial conditions - flow to the rigth
-	for (int i = 0; i < Nx; i++)
-		for (int j = 0; j < Ny; j++)
-			for (int a = 0; a < 9; a++)
-			{
-				f[i + Ny*(j + Q*a)] = (a != 1) ? 1.0 + 0.01*rand() : 2.0 * (1.0 + 0.2*cos(2*PI*i/Nx*4));
-				ftemp[i + Ny*(j + Q*a)] = 0.0;  // f[i + Ny*(j + Q*a)];
-			}
+	for (int j = 0; j < Ny; j++)
+		for (int i = 0; i < Nx; i++)
+		{
+			int pos = i + Nx*j;
+			rho[pos] = 1.;
+			u_x[pos] = 1.;
+			u_y[pos] = 0.;
+		}
 
+	for (int j = 0; j < Ny; j++)
+		for (int i = 0; i < Nx; i++)
+		{
+			int pos = i + Nx*j;
+			for (int a = 0; a < Q; a++)
+			{
+				f[Q*pos + a] = (a == 1) ? 2 : 1;
+				ftemp[pos*Q + a] = f[pos*Q + a];
+			}
+		}
 
 	// simulation main loop
 	int it = 0, out_cnt = 0;
 	while (it < iterations)
 	{
-		// calculate macroscopic quantities
-		for (int i = 0; i < Nx; i++)
-			for (int j = 0; j < Ny; j++)
-			{
-				u_x[i + Ny*j] = 0.0;
-				u_y[i + Ny*j] = 0.0;
-				rho[i + Ny*j] = 0.0;
-				for (int a = 0; a < Q; a++)
-				{
-					u_x[i + Ny*j] += ex[a] * f[i + Ny*(j + Q*a)];
-					u_y[i + Ny*j] += ey[a] * f[i + Ny*(j + Q*a)];
-					rho[i + Ny*j] +=         f[i + Ny*(j + Q*a)];
-				}
-				u_x[i + Ny*j] /= rho[i + Ny*j];
-				u_y[i + Ny*j] /= rho[i + Ny*j];
-			}
-		
+
 		// streaming step - periodic boundary conditions
-		for (int i = 0; i < Nx; i++)
+		for (int j = 0; j < Ny; j++)
 		{
-			int in = (i>0   ) ? (i-1) : (Nx-1);
-			int ip = (i<Nx-1) ? (i+1) : (0   );
+			int jn = (j>0) ? (j-1) : (Ny-1);
+			int jp = (j<Ny-1) ? (j+1) : (0);
 
-			for (int j = 0; j < Ny; j++)
+			for (int i = 0; i < Nx; i++)
 			{
-				if (!solid_node[i + Ny*j])
+				int pos = i + Nx*j;
+				if (!solid_node[pos])
 				{
-					int jn = (j>0) ? (j-1) : (Ny-1);
-					int jp = (j<Ny-1) ? (j+1) : (Ny-1);
+					int in = (i>0) ? (i-1) : (Nx-1);
+					int ip = (i<Nx-1) ? (i+1) : (0);
 
-					ftemp[i  + Ny*(j     )] = f[i + Ny*(j    )];
-					ftemp[ip + Ny*(j  + 1)] = f[i + Ny*(j + 1)];
-					ftemp[i  + Ny*(jp + 2)] = f[i + Ny*(j + 2)];
-					ftemp[in + Ny*(j  + 3)] = f[i + Ny*(j + 3)];
-					ftemp[i  + Ny*(jn + 4)] = f[i + Ny*(j + 4)];
-					ftemp[ip + Ny*(jp + 5)] = f[i + Ny*(j + 5)];
-					ftemp[in + Ny*(jp + 6)] = f[i + Ny*(j + 6)];
-					ftemp[in + Ny*(jn + 7)] = f[i + Ny*(j + 7)];
-					ftemp[ip + Ny*(jn + 8)] = f[i + Ny*(j + 8)];
-				}
-				else
-				{
-					// bounceback boundary conditions
-					float temp;
-					temp = f[i + Ny*(j + 1)]; f[i + Ny*(j + 1)] = f[i + Ny*(j + 3)]; f[i + Ny*(j + 3)] = temp;
-					temp = f[i + Ny*(j + 2)]; f[i + Ny*(j + 2)] = f[i + Ny*(j + 4)]; f[i + Ny*(j + 4)] = temp;
-					temp = f[i + Ny*(j + 5)]; f[i + Ny*(j + 5)] = f[i + Ny*(j + 7)]; f[i + Ny*(j + 7)] = temp;
-					temp = f[i + Ny*(j + 6)]; f[i + Ny*(j + 6)] = f[i + Ny*(j + 8)]; f[i + Ny*(j + 8)] = temp;
+					ftemp[Q*(i  + Nx*j )    ] = f[Q*pos    ];
+					ftemp[Q*(ip + Nx*j ) + 1] = f[Q*pos + 1];
+					ftemp[Q*(i  + Nx*jp) + 2] = f[Q*pos + 2];
+					ftemp[Q*(in + Nx*j ) + 3] = f[Q*pos + 3];
+					ftemp[Q*(i  + Nx*jn) + 4] = f[Q*pos + 4];
+					ftemp[Q*(ip + Nx*jp) + 5] = f[Q*pos + 5];
+					ftemp[Q*(in + Nx*jp) + 6] = f[Q*pos + 6];
+					ftemp[Q*(in + Nx*jn) + 7] = f[Q*pos + 7];
+					ftemp[Q*(ip + Nx*jn) + 8] = f[Q*pos + 8];
 				}
 			}
 		}
 
-		// calculate equilibrium distribution feq
-		float c1 = 3.0;
-		float c2 = 9./2.;
-		float c3 = 3./2.;
-		for (int i = 0; i < Nx; i++)
-			for (int j = 0; j < Ny; j++)
+		// calculate macroscopic quantities
+		for (int j = 0; j < Ny; j++)
+			for (int i = 0; i < Nx; i++)
 			{
-				if (!solid_node[i + Ny*j])
+				int pos = i + Nx*j;
+				u_x[pos] = 0.0;
+				u_y[pos] = 0.0;
+				rho[pos] = 0.0;
+				if (!solid_node[pos])
 				{
-					float rhoij = rho[i + Ny*j];
-					float w_rho0 = 4./9.  * rhoij;
-					float w_rho1 = 1./9.  * rhoij;
-					float w_rho2 = 1./36. * rhoij;
+					for (int a = 0; a < Q; a++)
+					{
+						u_x[pos] += ex[a] * ftemp[Q*pos + a];
+						u_y[pos] += ey[a] * ftemp[Q*pos + a];
+						rho[pos] +=         ftemp[Q*pos + a];
+					}
+					u_x[pos] /= rho[pos];
+					u_y[pos] /= rho[pos];
+				}
+			}
+		
+		// calc equilibrium function
+		calc_eq(rho, u_x, u_y, feq);
 
-					float uxij = u_x[i + Ny*j];
-					float uyij = u_y[i + Ny*j];
-
-					float uxsq = uxij * uxij;
-					float uysq = uyij * uyij;
-					float usq = uxsq + uysq;
-
-					float uxuy5 =  uxij + uyij;
-					float uxuy6 = -uxij + uyij;
-					float uxuy7 = -uxij - uyij;
-					float uxuy8 =  uxij - uyij;
-
-					float c4 = c3*usq;
-
-					feq[i + Ny*(j)      ] = w_rho0*(1.                             - c4);
-					feq[i + Ny*(j + Q*1)] = w_rho1*(1. + c1*uxij  + c2*uxsq        - c4);
-					feq[i + Ny*(j + Q*2)] = w_rho1*(1. + c1*uyij  + c2*uysq        - c4);
-					feq[i + Ny*(j + Q*3)] = w_rho1*(1. - c1*uxij  + c2*uxsq        - c4);
-					feq[i + Ny*(j + Q*4)] = w_rho1*(1. - c1*uyij  + c2*uysq        - c4);
-					feq[i + Ny*(j + Q*5)] = w_rho2*(1. + c1*uxuy5 + c2*uxuy5*uxuy5 - c4);
-					feq[i + Ny*(j + Q*6)] = w_rho2*(1. + c1*uxuy6 + c2*uxuy6*uxuy6 - c4);
-					feq[i + Ny*(j + Q*7)] = w_rho2*(1. + c1*uxuy7 + c2*uxuy7*uxuy7 - c4);
-					feq[i + Ny*(j + Q*8)] = w_rho2*(1. + c1*uxuy8 + c2*uxuy8*uxuy8 - c4);
+		// collision step
+		for (int j = 0; j < Ny; j++)
+			for (int i = 0; i < Nx; i++)
+			{
+				int pos = i + Nx*j;
+				if (!solid_node[pos])
+				{
+					for (int a = 0; a < Q; a++)
+					{
+						int ija = Q*pos + a;
+						f[ija] = ftemp[ija] - (ftemp[ija] - feq[ija]) / tau;
+					}
+				}
+				else
+				{
+					// bounceback boundary conditions
+					f[Q*pos + 1] = ftemp[Q*pos + 3];
+					f[Q*pos + 2] = ftemp[Q*pos + 4];
+					f[Q*pos + 3] = ftemp[Q*pos + 1];
+					f[Q*pos + 4] = ftemp[Q*pos + 2];
+					f[Q*pos + 5] = ftemp[Q*pos + 7];
+					f[Q*pos + 6] = ftemp[Q*pos + 8];
+					f[Q*pos + 7] = ftemp[Q*pos + 5];
+					f[Q*pos + 8] = ftemp[Q*pos + 6];
 				}
 			}
 
-		// collision step
-		for (int i = 0; i < Nx; i++)
-			for (int j = 0; j < Ny; j++)
-				if (!solid_node[i + Ny*j])
-					for (int a = 0; a < 9; a++)
-					{
-						int ija = i + Ny*(j + Q*a);
-						f[ija] = ftemp[ija] - (ftemp[ija] - feq[ija]) / tau;
-					}
-
-		if (it % 1 == 0)
+		if (it % 50 == 0)
 		{
 			cout << "iteration: " << it << "\toutput: " << out_cnt << endl;
 			grid_to_file(out_cnt, u_x, u_y, Nx, Ny);
