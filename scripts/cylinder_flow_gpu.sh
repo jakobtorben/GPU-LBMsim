8b320eea81f4681a2da50fcec9fd47b4cd6f79ce
acse-jrt3817 <jrt3817@ic.ac.uk>
2021-07-25 12:09:52 +0100

    Update GPU script

--- scripts/cylinder_flow_gpu.sh
+++ scripts/cylinder_flow_gpu.sh
@@ -1,7 +1,7 @@
-cd ../src
-nvcc -arch sm_61 -v --ptxas-options=-v -I../include -O3 core_gpu.cu init_gpu.cu utils.cpp main_gpu.cu -o LBM_sim
-#nvcc -gencode arch=compute_61,code=sm_61 saxpy.cu
-#cmake .. -DUSE_GPU=ON 
-#cd ../build
 cd ..
-src/LBM_sim input/benchmark.in
+mkdir build
+cd build
+cmake -DPERIODIC=OFF ..
+make GPU_target
+cd ..
+bin/GPU_target input/benchmark.in
